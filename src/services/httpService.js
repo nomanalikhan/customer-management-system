379b0653de462900f7971da9551358defabb001c
Noman Ali <4nauman@gmail.com>
2018-06-27 13:31:44 +0500

    introduce generic module for http-services

---
+++ src/services/httpService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const CancelToken = axios.CancelToken;
+
+const instance = axios.create({
+  baseURL: "/",
+  timeout: 35000,
+});
+
+export function get(url, params) {
+  return instance.get(url, params);
+}
+
+export function post(url, params) {
+  return instance.post(url, params);
+}
+
+export function put(url, params) {
+  return instance.put(url, params);
+}
+
+export function remove(url, params) {
+  return instance.delete(url);
+}
