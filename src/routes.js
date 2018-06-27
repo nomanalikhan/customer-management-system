ad1cccb21f468153d0598dc1a02c8672eab6e72b
Noman Ali <4nauman@gmail.com>
2018-06-28 03:54:07 +0500

    push a revised build on heroku

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 // import components for routing
 import MainLayout from "./components/MainLayout";
@@ -21,8 +26,10 @@ const _renderMainLayout = props => (
 
 // configure all the routes here inside a swtich statement and export them as a single component
 export const Views = () => (
-  <Switch>
-    <Route path="/" render={_renderMainLayout} />
-    <Redirect exact to="/customers" />
-  </Switch>
+  <Router>
+    <Switch>
+      <Route path="/" render={_renderMainLayout} />
+      <Redirect exact to="/customers" />
+    </Switch>
+  </Router>
 );
