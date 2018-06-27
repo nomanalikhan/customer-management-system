790075d257ac3e7aaf053bd14eaa33fde34d768b
Noman Ali <4nauman@gmail.com>
2018-06-27 23:57:10 +0500

    update date format methods to handle null data

--- src/utils/customer.js
+++ src/utils/customer.js
@@ -1,11 +1,11 @@
 import moment from "moment";
 
 export function formatBirthDate(val) {
-  return moment(val).format("DD MMMM YYYY");
+  return val ? moment(val).format("DD MMMM YYYY") : "";
 }
 
 export function formatLastContact(val) {
-  return moment(val).format("LLLL");
+  return val ? moment(val).format("LLLL") : "";
 }
 
 export function getGender(val) {
