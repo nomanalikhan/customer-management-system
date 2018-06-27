1c7d314415a6c13400a063d9dbf777dd28bf1c01
Noman Ali <4nauman@gmail.com>
2018-06-27 22:38:58 +0500

    reset previous user details from store on every get user detail api call

--- src/reducers/customersList.reducer.js
+++ src/reducers/customersList.reducer.js
@@ -5,7 +5,8 @@ import {
   GET_CUSTOMER_DETAILS_SUCCESS,
   GET_CUSTOMER_DETAILS_FAILED,
   CREATE_NEW_CUSTOMER_SUCCESS,
-  UPDATE_CUSTOMER_DETAILS_SUCCESS
+  UPDATE_CUSTOMER_DETAILS_SUCCESS,
+  GET_CUSTOMER_DETAILS
 } from "../actions/constants";
 
 const initialState = fromJS({
@@ -16,16 +17,20 @@ const initialState = fromJS({
 function customersListReducer(state = initialState, action) {
   switch (action.type) {
     case GET_CUSTOMERS_SUCCESS:
-      return state.set("customersList", action.payload.customersList);
+      return state
+        .set("customersList", action.payload.customersList)
+        .set("details", fromJS({}));
     case GET_CUSTOMERS_FAILED:
       return state.set("customersList", fromJS([]));
     case GET_CUSTOMER_DETAILS_SUCCESS:
       return state.set("details", action.payload.details);
     case GET_CUSTOMER_DETAILS_FAILED:
-      return state.set("details", fromJS([]));
+      return state.set("details", fromJS({}));
+    case GET_CUSTOMER_DETAILS:
     case CREATE_NEW_CUSTOMER_SUCCESS:
-    case UPDATE_CUSTOMER_DETAILS_SUCCESS:
-      return state.set("details", fromJS([]));
+    case UPDATE_CUSTOMER_DETAILS_SUCCESS: {
+      return state.set("details", fromJS({}));
+    }
 
     default:
       return state;
